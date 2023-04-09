f7107362d62f89efaab1a07a8bd115691f363cd7
RDJ-934 <rudrajoshi0903@gmail.com>
2023-04-09 16:32:47 +0530

    New added on 9th april at 4:32 pm

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,9 +10,8 @@ import { SidebarContext } from '../contexts/SidebarContext';
 
 import { CartContext } from '../contexts/CartContext';
 const Sidebar = () => {
-
   const { isOpen, handleClose } = useContext(SidebarContext);
-  console.log(useContext(CartContext));
+  // const { cart }= useContext(CartContext);
   return (<div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white h-full fixed top-0 shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
     <div className='flex justify-between items-center py-6 border-b'>
       <div className='uppercase font-semibold text-sm'>
@@ -22,7 +21,13 @@ const Sidebar = () => {
         <IoMdArrowForward className='text-2xl' />
       </div>
     </div>
-  </div>);
+     {/* <div>
+      {cart.map((item) => {
+        return <div>Cart Items</div>
+      })}
+     </div> */}
+  </div>
+  );
 };
 
 export default Sidebar;
