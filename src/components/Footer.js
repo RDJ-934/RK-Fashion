0ca65f9f255d5371f30e92aa16012a17ef349bbc
Kapadiya4923 <niyatikapadiya4923@gmail.com>
2023-04-18 13:51:41 +0530

    Fotter form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Footer = () => {
   return <footer className='bg-slate-500 py-12'>
-    <div className='container mx-auto'>
-      <p className='text-white text-center'>Copyright &copy; E-commerce Shop 2023. All Right Reserved</p>
+    <div className='container mx-auto gap-5 '>
+      <form className=' flex flex-col gap-2' action='#' method='POST'>
+      <input className='py-1 px-1 text-base w-[200px] ml-20'required type='text' placeholder='First Name'/>
+      <input className='py-1 px-1 text-base w-[275px] ml-20' required type='email' placeholder='Enter E-mail'/>
+      <textarea className='py-1 px-1 w-[400px] ml-20' cols={30} rows={6} required autoComplete="off" placeholder='Enter your Message'/>
+      <button className='px-2 py-2 w-[100px] ml-20 bg-primary text-white hover:bg-black transition duration:200ns'>Submit</button>
+      </form>
+
+      <p className='text-white text-center '>Copyright &copy; E-commerce Shop 2023. All Right Reserved</p>
     </div>
   </footer>;
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import ManImg from '../images/Men.png';
 const Hero = () => {
-  return <section className='bg-slate-50 h-[500px] py-24 mt-20'>
+  return <section className='bg-slate-50 h-[650px] py-24 mt-20'>
     <div className='container mx-auto flex justify-around h-full'>
       <div className='hidden lg:block'>
-        <img src={ManImg} alt=''/>
+        <img className='w-[750px] h-[400px] mt-[50px]' src={ManImg} alt=''/>
       </div>
     </div>
   </section>;
