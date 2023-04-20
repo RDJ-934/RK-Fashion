7b22499ccadad046e523ce621338d89b85660d51
RDJ-934 <rudrajoshi0903@gmail.com>
2023-04-20 17:17:43 +0530

    Image Slider

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ManImg from '../images/Men.png';
+import Slider from './slider';
 const Hero = () => {
-  return <section className='bg-slate-50 h-[650px] py-24 mt-20'>
-    <div className='container mx-auto flex justify-around h-full'>
-      <div className='hidden lg:block'>
-        <img className='w-[750px] h-[400px] mt-[50px]' src={ManImg} alt=''/>
-      </div>
+  return <section className='bg-slate-50 h-[600px] py-18 mt-20'>
+    <div className='container mx-auto flex justify-around '>
+        <Slider/>
     </div>
   </section>;
 };
---
+++ src/components/SilderRender.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Slider from './slider';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Slider />
+  </React.StrictMode>
+);
