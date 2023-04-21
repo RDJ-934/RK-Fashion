a465ac9ead3c3ce39a2878ab9d752320f214e3fd
RDJ-934 <rudrajoshi0903@gmail.com>
2023-04-21 10:53:25 +0530

    Size update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Slider from './slider';
 const Hero = () => {
-  return <section className='bg-slate-50 h-[600px] py-18 mt-20'>
+  return <section className='bg-slate-50 h-50 py-18 mt-20'>
     <div className='container mx-auto flex justify-around '>
         <Slider/>
     </div>
